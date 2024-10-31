3c2ff35b2da02a68c120b40e07c5a669642f2336
Hrishikesh Kannikar <hrishikeshkannikar@Hrishikeshs-MacBook-Air.local>
2024-11-01 00:03:50 +0530

    scopping global css to component specific

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../shared/button/button.component";
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
