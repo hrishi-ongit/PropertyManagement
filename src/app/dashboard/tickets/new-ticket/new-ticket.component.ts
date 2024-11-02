6a8c682da5284c1a51715264271e30ac6a88b267
Hrishikesh Kannikar <hrishikeshkannikar@Hrishikeshs-MacBook-Air.local>
2024-11-03 00:47:35 +0530

    Various types of template bindings and data accessing from template

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,12 +9,36 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(input: HTMLInputElement){
+  // @ViewChild('formRef') form?: ElementRef<HTMLFormElement>;
+
+  //Data binding with Template :-
+  //1.Two way binding with [(ngModel)]
+  
+  //2.Template variables and refference's
+  //Store a reference in the template as #variable and pass via method with type HTMLFormElement
+  // onSubmit(input: HTMLInputElement){
+  //   // onSubmit(input: any or string..excepted this way when tempalte sends titleInput.value){
+  //   console.log(input.value);
+  //   }
+
+  onSubmit(input: HTMLInputElement, form : HTMLFormElement){
+    // onSubmit(input: any or string..excepted this way when tempalte sends titleInput.value){
     console.log(input.value);
+    form.reset();
   }
 
-  //Template variables and refference's
-  //One way..two way binding with [(ngModel)]
-  //alternative way..store or reference in the template #variable
+  
+
+  //3.@ViewChild of type ElementRef<HTMLFormElement>; line 12
+  // onSubmit(input: HTMLInputElement){
+  //     // onSubmit(input: any or string..excepted this way when tempalte sends titleInput.value){
+  //     console.log(input.value);
+  //     this.form?.nativeElement.reset();
+  //   }
+
+  // 4.@ContentChild('input') private control: ElementRef<HTMLInputElement | HTMLTextAreaElement>//this is control.ts
+  // and in app-control template >> use the reference variable ..#var to the expected element
+  
+
 
 }
